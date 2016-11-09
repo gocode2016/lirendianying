e4ac9c60efcf70a7b1fd86e2cbc5ac865ef2b091
root <zjy_zs@163.com>
2016-11-09 22:38:54 +0800

    10.8

--- handle.py
+++ handle.py
@@ -6,8 +6,10 @@ import web
 import hashlib
 import reply
 import receive
+import mysql
 
 class Handle(object):
+    count = 0
     def GET(self):
         try:
             data = web.input()
@@ -35,13 +37,22 @@ class Handle(object):
     def POST(self):
         try:
             webData = web.data()
+            mysqlData = mysql.Select()
             print "Handle Post webdata is ", webData   #后台打日志
             recMsg = receive.parse_xml(webData)
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "test"
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                results_name = mysqlData.SelectData("movie_name")
+                results_url = mysqlData.SelectData("movie_url")
+                #for content in results_name:
+                Handle.count += 1
+                if Handle.count == 3:
+                    Handle.count = 0
+                content_gb = "电影名:" + results_name[Handle.count][0].encode('utf-8') + "链接：" + results_url[Handle.count][0].encode('utf-8')
+                replyMsg = reply.TextMsg(toUser, fromUser, content_gb)
+                    #replyMsg.send()
+                #return
                 return replyMsg.send()
             else:
                 print "暂且不处理"
--- mysql.py
+++ mysql.py
@@ -3,18 +3,20 @@
 import MySQLdb
 
 class Select():
-    def SelectData(self):
-        self.conn = MySQLdb.connect(host="139.196.29.97", db='movie', user='root', passwd='root', port=3306, charset='utf8', use_unicode=True)
+    def SelectData(self,key):
+        self.conn = MySQLdb.connect(host="139.196.29.97", db='movie', user='root', passwd='root', port=3306, charset= 'utf8', use_unicode=True)
         self.cur = self.conn.cursor()
 
-        sql = "select * from best_movies"
+        sql = "select %s from best_movies" %key
         self.cur.execute(sql)
 
         results = self.cur.fetchall()
 
-        for row in results:
-            id = row[0]
-            infp = row[1]
-            print "%s, %s" %(id, info)
+       # for row in results:
+      #      id = row[0]
+      #      info = row[1]
+	#    url = row[9]
+         #   print "%s, %s" %(id, info)
         self.cur.close()
         self.conn.close()
+        return results
